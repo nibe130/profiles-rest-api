e93fc59ec35ba0f7dc8ac149d38a5582d9da7ee8
nibe130 <ebin130@gmail.com>
2020-05-03 18:22:08 -0400

    Added rest methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 class HellowApiView(APIView):
     """Test API View"""
-    
+    serializer_class= serializers.HelloSerializers
+
     def get(self,request,format=None):
         """REturns a list  of APIView features"""
 
@@ -12,4 +15,36 @@ class HellowApiView(APIView):
         'I hope to do well']
 
         return Response({'message':'Hello','an_apiview':an_apiview})
-# Create your views here.
+    
+    def post(self,request):
+        """create a hello message with the passed in name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message' : message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+    #pk is the primary key id of the object we are updating
+    def put(self,response,pk=None):
+        """Handle updating objects"""
+        return Response({'method':'PUT'})
+
+    #updates partially onl the fields provided in the patch request. Ex only last name if 
+    #if you use put last name the entrire object will be updated the first name will be empty
+    #if you use patch the first name will be left as it is only the second name will be updated
+    def patch(self,request,pk=None):
+        """Handle partial update of an object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        """Handle deletion of an object"""
+        return Response({'method':'DELETE'})
+
+    
+
+    # Create your views here.
